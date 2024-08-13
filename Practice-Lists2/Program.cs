ab3a2f9662f3ec5564fe8f5d3a92a541753be150
FurkanCsk <furkancoskun99@hotmail.com>
2024-08-13 11:56:23 +0300

    Add project files.

---
+++ Practice-Lists2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Create an empty list to store coffee names
+List<string> coffee = new List<string>();
+
+// Define the number of coffee names to be entered
+int numberofCoffee = 5;
+
+// Loop to get coffe names from the user
+for (int i = 0;i < numberofCoffee;i++)
+{
+    Console.Write($"Coffee {i+1}: ");
+    coffee.Add(Console.ReadLine());
+}
+
+Console.WriteLine("\nThe entered coffee names:");
+
+// Loop to print the entered coffe names
+for (int i = 0; i < coffee.Count; i++)
+{
+    Console.WriteLine(coffee[i]);
+}
